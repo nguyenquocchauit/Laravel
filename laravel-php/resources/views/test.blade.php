0727e68a61a22ec08e7907a7e104b40c75283252
Nguyen Quoc Chau <chauquocnguyen.cun1@gmail.com>
2022-11-23 00:03:26 +0700

    update

--- laravel-php/resources/views/test.blade.php
+++ laravel-php/resources/views/test.blade.php
@@ -1,2 +1,7 @@
 <h1 style="text-align:center;">Test laravel - Nguyễn Quốc Châu - Fix Error 500 Server</h1>
-<?php echo phpversion() ?>
+
+<div style="text-align:center;">
+<strong>using command 'composer update'</strong> <br>
+<strong> create new file .env and copy data .env.example file to .env file </strong> <br>
+<strong> using command 'php artisan key:generate'</strong> <bsr>
+</div>
